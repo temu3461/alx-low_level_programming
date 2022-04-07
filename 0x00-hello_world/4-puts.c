3cbd84d93d27e475ee2c5fceb2d03e66f4dd597c
temesgen <temumujahid@gmail.com>
2022-04-07 05:33:56 -0700

    my 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+*the comment file
+*
+*return : 0 after printing 
+   */
 int main(void)
 {
-	puts (""Programming is like building a multilingual puzzle);
+	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
