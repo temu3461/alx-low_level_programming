64c883d42083640714bfb53031557e13857d3b22
temesgen <temumujahid@gmail.com>
2022-04-07 05:55:26 -0700

    my file 2

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.c>
+/**
+  *main - entry point
+  *
+  *Return : 0 after printing the function
+  */
+int main(void)
+{
+	printf(" with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
