aea44458e7b246a768cb5c0f4a9da5503ce8d302
temesgen <temumujahid@gmail.com>
2022-04-08 01:19:14 -0700

    hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/**
-  *main - entry point
-  *
-  *Return : 0 after success function
-  *
-  */
+ /**
+     *main - entry point
+     *
+     *Return : 0 after success function
+     *
+     */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
