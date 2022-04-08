55ee2012d121f373c624a44dc7e4cf26388e2058
temesgen <temumujahid@gmail.com>
2022-04-08 00:47:23 -0700

    my commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *
+  *Return : 0 after compilation
+  *
+  */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of n int : %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("size of a float : %d byte(s)\n", sizeof(float));
+     	return(0);
+}
