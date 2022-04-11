cd6d3d555048914c0bcc0303c5dc793abd3271c3
temesgen <temumujahid@gmail.com>
2022-04-11 04:59:34 -0700

    final else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-  * main - entry of the code 
+  * main - entry of the code
   *
   * Return - 0 after code success
   *
   */
-
+ 
 int main(void)
 {
 	int n;
